621e6435602ef1079c3c8670a553a3da57181128
ebraq8 <ebraheemalaamer95@gmail.com>
2025-03-28 00:09:59 -0500

    Fixed test build by separating temperature logic from main

--- problem2/temperature.h
+++ problem2/temperature.h
@@ -1,9 +1,17 @@
-#ifndef TEMPERATURE_H
-#define TEMPERATURE_H
+#include "temperature.h"
 
-float celsius_to_fahrenheit(float celsius);
-float fahrenheit_to_celsius(float fahrenheit);
-float celsius_to_kelvin(float celsius);
-float kelvin_to_celsius(float kelvin);
+float celsius_to_fahrenheit(float celsius) {
+    return (9.0/5.0) * celsius + 32;
+}
 
-#endif // TEMPERATURE_H
+float fahrenheit_to_celsius(float fahrenheit) {
+    return (5.0/9.0) * (fahrenheit - 32);
+}
+
+float celsius_to_kelvin(float celsius) {
+    return celsius + 273.15;
+}
+
+float kelvin_to_celsius(float kelvin) {
+    return kelvin - 273.15;
+}
